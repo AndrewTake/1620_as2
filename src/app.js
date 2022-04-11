b969a487afc8c095e0848bed4195e43b2371e8b4
AndrewTake <andrew6041@gmail.com>
2022-04-11 15:32:39 -0700

    cloned project and started JS. created a cancel button and save button.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 const notes = [
-  { 
-    title: "first note", 
+  {
+    title: "first note",
     noteBody: "this is an example note",
-    id: 1 
+    id: 1
   }
 ]
+const noteArea = document.querySelector(".write-note-area")
+const btn = document.querySelector(".icons")
+
+const displayNote = () => {
+  noteArea = document.querySelector('write-note-area')
+
+  const cancelBtn = document.querySelector('.cancelBtn')
+  const saveBtn = document.querySelector('.saveBtn')
+
+  cancelBtn.addEventListener('click', cancelNote)
+  saveBtn.addEventListener("click", saveNote)
+
+
